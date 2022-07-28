62af59eabaf34f1d35dfaefd0dd917eaa581d6f7
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-07-28 21:01:45 +0300

    file movie.js

--- application/script.js
+++ application/script.js
@@ -8,11 +8,28 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Last movie?', '');
-const c = prompt('Last movie?', '');
-const b = prompt('Grade ? ', '');
-const d = prompt('Grade ? ', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
+for(let i = 0; i<2; i++){
+    const a = prompt('Last movie?', '');
+    const b = prompt('Grade ? ', '');
+    personalMovieDB.movies[a] = b;
+
+if (a != '' && b != '' && a != null && b != null && a.length < 50){
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+}else{
+    console.log('error');
+    i--;
+}
+
+}
+if (numberOfFilms < 10){
+    console.log("Not enough movies");
+} else if (numberOfFilms >=10 && numberOfFilms < 30){
+    console.log("classic human");
+} else if (numberOfFilms > 30){
+    console.log('cinemaman');
+}else{
+    console.log("Error");
+}
