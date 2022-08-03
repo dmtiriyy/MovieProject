e41a8fafe2b1c5061dad56d718380fff9b29b7b1
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-08-03 18:45:26 +0300

    add functions to simplify db

--- application/script.js
+++ application/script.js
@@ -1,6 +1,26 @@
-const numberOfFilms = prompt('Сколько фильмов? ','');
+let numberOfFilms;
 
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов? ','');
+    while (numberOfFilms =="" || numberOfFilms == null|| isNaN(numberOfFilms)){
+        numberOfFilms = prompt('Сколько фильмов? ','');
+    }
+}
+start();
+/* Задание на урок:
 
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+// Код возьмите из предыдущего домашнего задания
 const personalMovieDB = {
     count: numberOfFilms,
     movies :{},
@@ -8,28 +28,50 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-
+    
 console.log(personalMovieDB);
-for(let i = 0; i<2; i++){
-    const a = prompt('Last movie?', '');
-    const b = prompt('Grade ? ', '');
-    personalMovieDB.movies[a] = b;
+function writeYourGenres(){
+    let favouriteGenre;
+    for(let i=1;i<=3; i++){
+        favouriteGenre = prompt(`Ваш любимый жанр под номером ${i}`,'');
+        personalMovieDB.genres[i-1]=favouriteGenre;
+    }
+    
+}
+writeYourGenres();
+function showMyDB(private){
+    if (!private){
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+function rememberMyFilms(){
+    for(let i = 0; i<2; i++){
+        const a = prompt('Last movie?', '');
+        const b = prompt('Grade ? ', '');
+        personalMovieDB.movies[a] = b;
+    
+    if (a != '' && b != '' && a != null && b != null && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }else{
+        console.log('error');
+        i--;
+    }
+}
+}
+//rememberMyFilms();
 
-if (a != '' && b != '' && a != null && b != null && a.length < 50){
-    personalMovieDB.movies[a] = b;
-    console.log('done');
-}else{
-    console.log('error');
-    i--;
+function detectPersonalLevel(){
+    if (numberOfFilms < 10){
+        console.log("Not enough movies");
+    } else if (numberOfFilms >=10 && numberOfFilms < 30){
+        console.log("classic human");
+    } else if (numberOfFilms > 30){
+        console.log('cinemaman');
+    }else{
+        console.log("Error");
+    }
 }
-
-}
-if (numberOfFilms < 10){
-    console.log("Not enough movies");
-} else if (numberOfFilms >=10 && numberOfFilms < 30){
-    console.log("classic human");
-} else if (numberOfFilms > 30){
-    console.log('cinemaman');
-}else{
-    console.log("Error");
-}
+//detectPersonalLevel();
+console.log(personalMovieDB);
--- movie.js
+++
@@ -1,5 +0,0 @@
-class Movie{
-    constructor(year){
-        
-    }
-}
